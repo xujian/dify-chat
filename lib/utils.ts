e313d98bb91d387d58113f1c81c08be759a38abe
xujian <mikejianxu@gmail.com>
2025-03-31 14:14:26 +0800

    make message history custom block

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,47 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function isJson(str: string) {
+  const input = str.trim()
+  if (!input) {
+    return false
+  }
+
+  if (!(
+    (input.startsWith('{') && input.endsWith('}')) ||
+    (input.startsWith('[') && input.endsWith(']'))
+  )) {
+    return false
+  }
+
+  try {
+    JSON.parse(str)
+    return true
+  }
+  catch (e) {
+    return false
+  }
+}
+
+export function toJson(str: string) {
+  const input = str.trim()
+  if (!input) {
+    return null
+  }
+
+  if (!(
+    (input.startsWith('{') && input.endsWith('}')) ||
+    (input.startsWith('[') && input.endsWith(']'))
+  )) {
+    return null
+  }
+
+  try {
+    return JSON.parse(str)
+  }
+  catch (e) {
+    return null
+  }
+}
