9d3b8eda21f7dd95a9f18656290b610700a6132c
xujian <mikejianxu@gmail.com>
2025-04-09 17:37:40 +0800

    fix loading style

--- app/components/chat/loading.tsx
+++ app/components/chat/loading.tsx
@@ -3,15 +3,15 @@ import type { FC } from 'react'
 import React from 'react'
 import s from './loading.module.css'
 
-export type ILoaidingAnimProps = {
+export type LoadingProps = {
   type: 'text' | 'avatar'
 }
 
-const LoaidingAnim: FC<ILoaidingAnimProps> = ({
+const Loading: FC<LoadingProps> = ({
   type,
 }) => {
   return (
     <div className={`${s['dot-flashing']} ${s[type]}`}></div>
   )
 }
-export default React.memo(LoaidingAnim)
+export default React.memo(Loading)
