1b20a09f5826f30f2caa439a62ad2c7da7d9fad3
xujian <mikejianxu@gmail.com>
2025-04-17 17:49:09 +0800

    type Field + onChange

--- models/form.ts
+++ models/form.ts
@@ -8,12 +8,14 @@ export type LabelValuePair = {
  */
 export type Field = {
   name: string
+  value: string
   label: string
   type: string
   required: boolean
   default?: string | number
   maxLength?: number
   options?: string[] | LabelValuePair[]
+  onChange?: (value: string) => void
 }
 
 export type Variable = Field & {
