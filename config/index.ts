4236770a48814176876015b74bb603b88adcfa59
xujian <mikejianxu@gmail.com>
2025-04-25 17:37:02 +0800

    + useHistory config

--- config/index.ts
+++ config/index.ts
@@ -8,11 +8,12 @@ export const APP_INFO: AppInfo = {
   copyright: '',
   privacyPolicy: '',
   defaultLanguage: 'zh-Hans',
-  server: `${process.env.NEXT_PUBLIC_API_URL}`.replace('/v1', '')
+  server: `${process.env.NEXT_PUBLIC_API_URL}`.replace('/v1', ''),
+  useHistory: false
 }
 
-export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+// export const isShowPrompt = false
+// export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
 
