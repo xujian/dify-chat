80852e28d7595553a75e61c214ffdf77c79c156b
gaozhu <gaozhu@ddjf.com.cn>
2025-04-28 10:29:43 +0800

    fix(config): 更新API前缀为'/dify-chat/api'
    为了与新的API路径保持一致，将API前缀从'/api'更改为'/dify-chat/api'

--- config/index.ts
+++ config/index.ts
@@ -15,7 +15,7 @@ export const APP_INFO: AppInfo = {
 // export const isShowPrompt = false
 // export const promptTemplate = 'I want you to act as a javascript console.'
 
-export const API_PREFIX = '/api'
+export const API_PREFIX = '/dify-chat/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
