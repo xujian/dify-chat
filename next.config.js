212e4df3147bbe57c3d2361c6ba9ad350ca08432
xujian <mikejianxu@gmail.com>
2025-05-08 09:50:25 +0800

    fix strictMode

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   productionBrowserSourceMaps: false, // enable browser source map generation during the production build
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
