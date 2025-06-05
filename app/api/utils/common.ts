920e3f6a72474592fc6adceb29758454f1916bfb
xujian <mikejianxu@gmail.com>
2025-06-05 15:35:50 +0800

    add user name

--- app/api/utils/common.ts
+++ app/api/utils/common.ts
@@ -3,11 +3,13 @@ import { ChatClient } from 'dify-client'
 import { v4 } from 'uuid'
 import { API_KEY, API_URL, APP_ID } from '@/config'
 
-const userPrefix = `user_${APP_ID}:`
+// const userPrefix = `user_${APP_ID}:`
 
 export const getInfo = (request: NextRequest) => {
-  const sessionId = request.cookies.get('session_id')?.value || v4()
-  const user = userPrefix + sessionId
+  const sessionId = request.cookies.get('session_id')?.value || v4(),
+    name = request.cookies.get('user')?.value || 'user'
+  // const user = `user_${APP_ID}:${sessionId}`
+  const user = `${name}_${APP_ID}:${sessionId}`
   return {
     sessionId,
     user,
